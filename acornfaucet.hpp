e83c7c07eded1867fa81942df40aff050c43dfc5
fcecin <fcecin@gmail.com>
2019-05-22 21:53:00 +0200

    Update acornfaucet.hpp
    fix comments

--- acornfaucet.hpp
+++ acornfaucet.hpp
@@ -1,20 +1,11 @@
 /**
- * This contract interacts with the 'acornaccount' ACORN token contract to allow
- *   'host' and 'player' clients to bet against each other.
- *
- * The 'host' offers commitments and reveals them. It can serve games as long as it has 
- *   a positive balance of deposits.
- *
- * Players play by simply transferring the token into this contract. The contract detects
- *   the transfer action and reacts accordingly, i.e. tries to immediately match it to a 
- *   host commitment. If it can't, the player's transfer is rejected.
+ * Simple ACORN faucet contract.
  */
 #pragma once
 
 #include <eosio/asset.hpp>
 #include <eosio/eosio.hpp>
 #include <eosio/transaction.hpp>
-//#include <eosio/crypto.hpp>
 
 #include <string>
 
